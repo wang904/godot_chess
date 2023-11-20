33d17f7ab6d86a4ce01dac46d81294638ec3381f
wang904 <56588029+wang904@users.noreply.github.com>
2023-11-20 16:22:23 +0800

    增加兵，王，马的移动

--- script/chessboard/pieces.gd
+++ script/chessboard/pieces.gd
@@ -26,13 +26,75 @@ var player : bool = true
 func select_piece():
 	var clicked_cell : Vector2i = local_to_map(get_local_mouse_position())
 	var cell_id : Cells = get_cell_source_id(0,clicked_cell)
- 	#6 是白棋和黑棋的交界处
+	#6是白棋和黑棋的交界处
 	if (player and cell_id >= 6) or (not player and cell_id < 6):
 		last_piece_position = clicked_cell
 		last_piece_col = cell_id
 
+func check_king(clicked_cell) -> bool:
+	var dis = clicked_cell - last_piece_position
+	return  abs(dis.x) <= 1 and abs(dis.y) <= 1 
+
+func has_piece(position):
+	return get_cell_source_id(0,position) != -1
+
+func check_white_pawn(clicked_cell) -> bool:
+	if last_piece_position.x == clicked_cell.x:
+		if last_piece_position.y == 6 and clicked_cell.y - last_piece_position.y == -2 and !has_piece(clicked_cell) and !has_piece(Vector2i(clicked_cell.x,clicked_cell.y + 1)):
+			return true
+		if  clicked_cell.y - last_piece_position.y == -1 and !has_piece(clicked_cell):
+			return true
+	if abs(last_piece_position.x - clicked_cell.x) == 1 and clicked_cell.y - last_piece_position.y == -1:
+		#这里不用判断吃自己子因为已经在can_move里面判断了
+		if has_piece(clicked_cell):
+			return true
+	return false
+
+func check_black_pawn(clicked_cell) -> bool:
+	if last_piece_position.x == clicked_cell.x:
+		if last_piece_position.y == 1 and clicked_cell.y - last_piece_position.y == 2 and !has_piece(clicked_cell) and !has_piece(Vector2i(clicked_cell.x,clicked_cell.y - 1)):
+			return true
+		if  clicked_cell.y - last_piece_position.y == 1 and !has_piece(clicked_cell):
+			return true
+	if abs(last_piece_position.x - clicked_cell.x) == 1 and clicked_cell.y - last_piece_position.y == 1:
+		#这里不用判断吃自己子因为已经在can_move里面判断了
+		if has_piece(clicked_cell):
+			return true
+	return false
+
+
+func check_knight(clicked_cell) -> bool:
+	var deltaX = [2, 1,-1,-2,-2,-1, 1, 2]
+	var deltaY = [1, 2, 2, 1,-1,-2,-2,-1];
+	for i in range(8):
+		print(deltaX[i]," ",deltaY[i])
+		if Vector2i(last_piece_position.x + deltaX[i],last_piece_position.y + deltaY[i]) == clicked_cell:
+			return true
+	return false
+
 #是否可以移动
-func can_move(_clicked_cell) -> bool:
+func can_move(clicked_cell) -> bool:
+	var cell_id : Cells = get_cell_source_id(0,clicked_cell)
+	#判断不能吃自己的子，不能不移动
+	if clicked_cell == last_piece_position:
+		return false
+	if (player and cell_id >= 6) or (not player and cell_id < 6 and cell_id != -1):
+		last_piece_position = clicked_cell
+		last_piece_col = cell_id
+		return false
+	match last_piece_col:
+		Cells.WHITE_KING,Cells.BLACK_KING:
+			if not check_king(clicked_cell):
+				return false
+		Cells.WHITE_PAWN:
+			if not check_white_pawn(clicked_cell):
+				return false
+		Cells.BLACK_PAWN:
+			if not check_black_pawn(clicked_cell):
+				return false
+		Cells.WHITE_KNIGHT,Cells.BLACK_KNIGHT:
+			if not check_knight(clicked_cell):
+				return false
 	return true
 
 #返回值代表移动是否成功
@@ -43,7 +105,6 @@ func make_move() -> bool:
 		set_cell(0,clicked_cell,last_piece_col,Vector2i(0,0))
 		set_cell(0,last_piece_position)
 		ok = true
-	if clicked_cell != last_piece_position:
 		last_piece_col = Cells.EMPTY
 		last_piece_position = Vector2i(-1,-1)
 	return ok
